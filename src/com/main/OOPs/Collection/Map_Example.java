3d99036dacc41ba3712730cd621c154ff3d6b3ad
ritowhid <207604574+ritowhid@users.noreply.github.com>
2025-07-11 03:41:02 +0600

    learning update

---
+++ src/com/main/OOPs/Collection/Map_Example.java
@@ -0,0 +1,33 @@
+package com.main.OOPs.Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_Example {
+    public static void main(String[] args) {
+        Map<String, Integer> std = new HashMap<>(); // if you want to use normal-use HashMap, if you want synchronized version-use Hashtable; better to use when have multiple thread working
+        std.put("Towhid", 43);
+        std.put("Alice", 44);
+        std.put("Bob", 49);
+        std.put("Jack", 52);
+        std.put("Alice", 59); // changing value
+
+        // .add mean - adding a new element, .put mean - try to add the element, if you already have this key, replace it.
+
+//        // output in maps
+//        System.out.println(std);
+//        // get specific data
+//        System.out.println(std.get("Towhid"));
+//
+//        // accessing all key
+//        System.out.println(std.keySet());
+        for (String key : std.keySet()) {
+            System.out.println(key + " : " + std.get(key)); //keySet() → Get the names (key) Example: "Towhid", "Alice", "Bob"
+        }                                                   // get(key) → Get the value for that name, Example: get("Towhid") → 43
+
+    }
+}
+/*
+    -> A Map stores key-value pairs. Each key is unique. Each key map to exactly one value.
+       It's not a subtype of Collection but part of the Collection Framework.
+ */
---
+++ src/com/main/OOPs/Collection/StreamAPI_example.java
@@ -0,0 +1,42 @@
+package com.main.OOPs.Collection;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamAPI_example {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(4, 5, 6, 7, 8);
+
+//        Stream<Integer> s1 = nums.stream();
+//        Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
+        /* filter returns a stream, when you say s1.filter;
+         it gives a new stream. also, stream s1 has already
+         been operated upon or closed after used s2. you can't print s1 now.
+        */
+//        Stream<Integer> s3 = s2.map(n -> n*2); // now s2 is done.
+//        s3.forEach(System.out::println);
+//        s1.forEach(System.out::println); // can't use stream more than one time.
+
+//        int result = s3.reduce(0, (c,e) -> c+e);
+//        System.out.println(result); // can't use her forEach, bcz result will give one value.
+
+        // we can perform this all operation more enhanced way:
+        int result = nums.stream()
+                .filter(n -> n%2==0) //filtered (even only): [4, 6, 8]
+                .map(n -> n*2)       // Mapped (each ×2): [8, 12, 16]
+                .reduce(0, (c,e) -> c+e); // Reduced (sum): 8 + 12 + 16 = 36
+        System.out.println(result);
+
+    }
+}
+/*
+    -> Why Use Stream API in Java?
+       Because it makes your code shorter, cleaner, and often faster
+       when working with collections like List, Set, etc.
+    -> Functional Style (like in Python or JavaScript).
+       You don’t modify data directly.
+       You chain operations (filter → map → collect) easily.
+    -> No Need for Loops.
+    -> Thread-safe option(parallel)
+ */
